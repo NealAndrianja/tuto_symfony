7a5a9581b947dcb0b7c3489468b744c3e2bfb2b3
Neal <mcandrineal@gmail.com>
2024-09-19 10:53:06 +0300

    signin success

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -2,21 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;    
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, ): JsonResponse
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SigninController.php',
-        ]);
+        $data = json_decode($request->getContent(), true);
+        if(!$data){
+            return new JsonResponse(['message' => 'Invalid credentials'], 400);
+        }
+
+        //new user
+        $user = new User();
+        $user->setEmail($data['email'] ?? '');
+        $user->setPassword($data['password'] ?? '');
+
+        //Validation
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $err) {
+                $errorMessages[] = $err->getMessage();
+            }
+            return new JsonResponse($errorMessages, 400);
+        }
+
+        //hash password
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        //persist user data
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        //return success response
+        return new JsonResponse(['message' => 'User created successfully'], 201);
     }
 }
